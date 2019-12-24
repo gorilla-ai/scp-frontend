5d67d38d42f77a357096f4d03316d95d92600c1a
Ryan Chen (Telmediatech) <ryanchen@telmediatech.com>
2019-12-24 18:07:19 +0800

    Change search bar interval options to dropdown list. Add value check for table field value.

--- src/components/common/search-options.js
+++ src/components/common/search-options.js
@@ -27,10 +27,6 @@ class SearchOptions extends Component {
 
     t = global.chewbaccaI18n.getFixedT(null, 'connections');
     et = global.chewbaccaI18n.getFixedT(null, 'errors');
-
-    this.state = {
-      intervalModalOpen: false
-    };
   }
   componentDidMount() {
     this.loadSearchOptions();
@@ -56,7 +52,7 @@ class SearchOptions extends Component {
 
     if (prevProps && prevProps.searchInput) {
       if (prevProps.searchInput.searchInterval !== searchInput.searchInterval) {
-        this.handleIntervalConfirm('noToggle');
+        this.handleIntervalConfirm();
       }
     }
 
@@ -73,88 +69,50 @@ class SearchOptions extends Component {
    * @method
    */
   setNewDatetime = () => {
-    const {searchInput} = this.props;
-    const dataObj = this.getTimeAndText(searchInput.searchInterval);
     const datetime = {
-      from: dataObj.time,
+      from: this.getCalculatedTime(this.props.searchInput.searchInterval),
       to: Moment().local().format('YYYY-MM-DDTHH:mm') + ':00'
     };
 
     this.props.handleDateChange(datetime, 'refresh');
   }
   /**
-   * Get text and time to be displayed in the search bar based on user's selection
+   * Get calculated time based on user's time selection
    * @method
    * @param {string} type - time options
-   * @returns text and time
+   * @returns calculated time
    */
-  getTimeAndText = (type) => {
-    let text = '';
+  getCalculatedTime = (type) => {
     let time = '';
 
     switch(type) {
       case '15m':
-        text = t('events.connections.txt-last15m');
         time = helper.getSubstractDate(15, 'minutes');
         break;
       case '30m':
-        text = t('events.connections.txt-last30m');
         time = helper.getSubstractDate(30, 'minutes');
         break;
       case '1h':
-        text = t('events.connections.txt-last1h');
         time = helper.getSubstractDate(1, 'hours');
         break;
       case '2h':
-        text = t('events.connections.txt-last2h');
         time = helper.getSubstractDate(2, 'hours');
         break;
       case '12h':
-        text = t('events.connections.txt-last12h');
         time = helper.getSubstractDate(12, 'hours');
         break;
       case '24h':
-        text = t('events.connections.txt-last24h');
         time = helper.getSubstractDate(24, 'hours');
         break;
       case 'today':
-        text = t('events.connections.txt-today');
         time = helper.getStartDate('day');
         break;
       case 'week':
-        text = t('events.connections.txt-week');
         time = helper.getStartDate('week');
-        break;
-      case '15000':
-        text = t('events.connections.txt-15s');
-        break;
-      case '30000':
-        text = t('events.connections.txt-30s');
-        break;
-      case '60000':
-        text = t('events.connections.txt-1m');
-        break;
-      case '300000':
-        text = t('events.connections.txt-5m');
-        break;
-      case '600000':
-        text = t('events.connections.txt-10m');
         break;
     }
 
-    return {
-      text,
-      time
-    };
-  }
-  /**
-   * Toggle interval dialog on/off
-   * @method
-   */
-  toggleIntervalDialog = () => {
-    this.setState({
-      intervalModalOpen: !this.state.intervalModalOpen
-    });
+    return time;
   }
   /**
    * Set search type and interval based on user's selection
@@ -165,9 +123,7 @@ class SearchOptions extends Component {
     this.props.setSearchData('all', {
       searchType: type,
       searchInterval: '1h',
-      refreshTime: '600000',
-      inputManual: t('events.connections.txt-last1h'),
-      inputAuto: t('txt-interval') + ': ' + t('events.connections.txt-10m')
+      refreshTime: '600000'
     });
 
     if (this.intervalId) {
@@ -176,115 +132,14 @@ class SearchOptions extends Component {
     }
   }
   /**
-   * Display interval options content
-   * @method
-   * @returns HTML DOM
-   */
-  displayIntervalOptions = () => {
-    const {searchInput} = this.props;
-
-    if (searchInput.searchType === 'manual') {
-      return (
-        <div className='interval-options manual'>
-          <label>{t('events.connections.txt-time-frame')}</label>
-          <RadioGroup
-            id='timeInterval'
-            list={[
-              {value: '30m', text: t('events.connections.txt-last30m')},
-              {value: '1h', text: t('events.connections.txt-last1h')},
-              {value: '2h', text: t('events.connections.txt-last2h')},
-              {value: 'today', text: t('events.connections.txt-today')},
-              {value: '24h', text: t('events.connections.txt-last24h')},
-              {value: 'week', text: t('events.connections.txt-week')}
-            ]}
-            onChange={this.props.setSearchData.bind(this, 'searchInterval')}
-            value={searchInput.searchInterval} />
-        </div>
-      )
-    } else if (searchInput.searchType === 'auto') {
-      return (
-        <div className='interval-options auto'>
-          <label>{t('events.connections.txt-time-frame')}</label>
-          <RadioGroup
-            id='timeInterval'
-            list={[
-              {value: '15m', text: t('events.connections.txt-last15m')},
-              {value: '30m', text: t('events.connections.txt-last30m')},
-              {value: '1h', text: t('events.connections.txt-last1h')},
-              {value: '12h', text: t('events.connections.txt-last12h')}
-            ]}
-            onChange={this.props.setSearchData.bind(this, 'searchInterval')}
-            value={searchInput.searchInterval} />
-
-          <label>{t('events.connections.txt-auto-update')}</label>
-          <RadioGroup
-            id='updateInterval'
-            list={[
-              {value: '15000', text: t('events.connections.txt-15s')},
-              {value: '30000', text: t('events.connections.txt-30s')},
-              {value: '60000', text: t('events.connections.txt-1m')},
-              {value: '300000', text: t('events.connections.txt-5m')},
-              {value: '600000', text: t('events.connections.txt-10m')}
-            ]}
-            onChange={this.props.setSearchData.bind(this, 'refreshTime')}
-            value={searchInput.refreshTime} />
-        </div>
-      )
-    }
-  }
-  /**
-   * Display interval options modal dialog
-   * @method
-   * @returns ModalDialog component
-   */
-  intervalModalDialog = () => {
-    const {activeTab} = this.state;
-    const actions = {
-      confirm: {text: t('txt-close'), handler: this.handleIntervalConfirm}
-    };
-    const titleText = t('events.connections.txt-time-frame');
-
-    return (
-      <ModalDialog
-        id='intervalModalDialog'
-        className='modal-dialog'
-        title={titleText}
-        draggable={true}
-        global={true}
-        actions={actions}
-        closeAction='confirm'>
-        {this.displayIntervalOptions()}
-      </ModalDialog>
-    )
-  }
-  /**
    * Set search data based on user's selection
    * @method
    */
-  handleIntervalConfirm = (options) => {
-    const {searchInput} = this.props;
-    let dataObj = this.getTimeAndText(searchInput.searchInterval);
-    let inputManual = '';
-    let inputAuto = '';
-
+  handleIntervalConfirm = () => {
     this.props.handleDateChange({
-      from: dataObj.time,
+      from: this.getCalculatedTime(this.props.searchInput.searchInterval),
       to: Moment().local().format('YYYY-MM-DDTHH:mm') + ':00Z'
     });
-
-    if (searchInput.searchType === 'manual') {
-      inputManual = dataObj.text;
-    } else if (searchInput.searchType === 'auto') {
-      dataObj = this.getTimeAndText(searchInput.refreshTime);
-      inputAuto = t('txt-interval') + ': ' + dataObj.text;
-    }
-
-    this.props.setSearchData('inputManual', inputManual);
-    this.props.setSearchData('inputAuto', inputAuto);
-
-    if (!options || options !== 'noToggle') {
-      this.toggleIntervalDialog(options);
-    }
   }
   /**
    * Display date picker
@@ -328,29 +183,10 @@ class SearchOptions extends Component {
   }
   render() {
     const {position, showFilter, showInterval, searchInput} = this.props;
-    const {intervalModalOpen} = this.state;
     const searchStyle = position ? position : '10px';
-    let searchManualText = '';
-    let searchAutoText = '';
-    let searchInputValue = '';
-
-    if (searchInput) {
-      searchManualText = searchInput.inputManual ? searchInput.inputManual : t('events.connections.txt-last1h');
-      searchAutoText = searchInput.inputAuto ? searchInput.inputAuto : t('txt-interval') + ': ' + t('events.connections.txt-10m');
-
-      if (searchInput.searchType === 'manual') {
-        searchInputValue = searchManualText;
-      } else if (searchInput.searchType === 'auto') {
-        searchInputValue = searchAutoText;
-      }
-    }
 
     return (
       <div className='search-options' style={{right: searchStyle}}>
-        {intervalModalOpen &&
-          this.intervalModalDialog()
-        }
-
         {showInterval &&
           <DropDownList
             className='search-type'
@@ -365,7 +201,48 @@ class SearchOptions extends Component {
 
         {showInterval &&
           <div className='search-interval'>
-            <input className='time-interval' style={{width: this.getInputWidth()}} value={searchInputValue} onClick={this.toggleIntervalDialog} readOnly />
+            {searchInput.searchType === 'auto' &&
+              <div>
+                <DropDownList
+                  id='updateInterval'
+                  list={[
+                    {value: '15000', text: t('events.connections.txt-15s')},
+                    {value: '30000', text: t('events.connections.txt-30s')},
+                    {value: '60000', text: t('events.connections.txt-1m')},
+                    {value: '300000', text: t('events.connections.txt-5m')},
+                    {value: '600000', text: t('events.connections.txt-10m')}
+                  ]}
+                  required={true}
+                  onChange={this.props.setSearchData.bind(this, 'refreshTime')}
+                  value={searchInput.refreshTime} />
+                <DropDownList
+                  id='timeInterval'
+                  list={[
+                    {value: '15m', text: t('events.connections.txt-last15m')},
+                    {value: '30m', text: t('events.connections.txt-last30m')},
+                    {value: '1h', text: t('events.connections.txt-last1h')},
+                    {value: '12h', text: t('events.connections.txt-last12h')}
+                  ]}
+                  required={true}
+                  onChange={this.props.setSearchData.bind(this, 'searchInterval')}
+                  value={searchInput.searchInterval} />          
+              </div>
+            }
+            {searchInput.searchType === 'manual' &&
+              <DropDownList
+                id='timeInterval'
+                list={[
+                  {value: '30m', text: t('events.connections.txt-last30m')},
+                  {value: '1h', text: t('events.connections.txt-last1h')},
+                  {value: '2h', text: t('events.connections.txt-last2h')},
+                  {value: 'today', text: t('events.connections.txt-today')},
+                  {value: '24h', text: t('events.connections.txt-last24h')},
+                  {value: 'week', text: t('events.connections.txt-week')}
+                ]}
+                required={true}
+                onChange={this.props.setSearchData.bind(this, 'searchInterval')}
+                value={searchInput.searchInterval} />
+            }
           </div>
         }
         
