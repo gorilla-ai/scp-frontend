b916719cf01601c84f1a3970a75a6b2a31a22747
Ryan Chen (Telmediatech) <ryanchen@telmediatech.com>
2020-08-06 14:24:08 +0800

    Reverse server.js file.

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const server = require('http').Server(app)
 const cfg = _.assign({
   env: process.env.NODE_ENV || 'development', 
   contextRoot: '',
-  //apiPrefix: 'http:\//172.18.0.119/SCP', //Prod environment
-  apiPrefix: 'http:\//192.168.11.91/SCP', //Test environment
+  apiPrefix: 'http:\//172.18.0.119/SCP', //Prod environment
+  //apiPrefix: 'http:\//192.168.11.91/SCP', //Test environment
   //apiPrefix: 'http:\//192.168.12.111/SCP', //QA environment
   //apiPrefix: 'http:\//172.18.0.136/SCP', //Rays's machine
   //apiPrefix: 'http:\//172.18.0.87/SCP', //Rex's machine
