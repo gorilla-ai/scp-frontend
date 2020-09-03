064edfc846861b22e7ec896ab1b3a1ad5a9267f4
Ken Lee (ns-guard) <ken.lee@gorillascience.com>
2020-09-03 16:57:46 +0800

    export dashboard pdf

--- src/components/dashboard/statisticsUIF.js
+++ src/components/dashboard/statisticsUIF.js
@@ -5,14 +5,20 @@ import PropTypes from 'prop-types'
 import Moment from 'moment'
 import _ from 'lodash'
 import cx from 'classnames'
+import Promise from 'bluebird'
 
 import DateRange from 'react-ui/build/src/components/date-range'
+import Progress from 'react-ui/build/src/components/progress'
 
+import SearchOptions from '../common/search-options'
 import {BaseDataContext} from '../common/context';
 import helper from '../common/helper'
 
+import {downloadLink} from 'react-ui/build/src/utils/download'
 import {default as ah, getInstance} from 'react-ui/build/src/utils/ajax-helper'
 import {HOC} from 'widget-builder'
+
+import htmlToImage from 'html-to-image'
 
 let t = null
 let et = null
@@ -23,8 +29,14 @@ const ALERT_LEVEL_COLORS = {Emergency: '#CC2943', Alert: '#CC7B29', Critical: '#
 const COLORS = ['#069BDA', '#57C3D9', '#57D998', '#6CD957', '#C3D957', '#D99857', '#D9576C', '#D957C3', '#9857D9', '#576CD9', '#5798D9', '#57D9C3', '#57D96C', '#98D957', '#D9C357', '#D96C57', '#D95798', '#C357D9', '#6C57D9']
 
 const INIT = {
+  uifCfg: {},
   appendConfig: {},
-  datetime: {}
+  datetime: {},
+  searchInput: {
+    searchType: 'manual',
+    searchInterval: '1h',
+    refreshTime: '600000' //10 minutes
+  }
 }
 
 class StatisticsUIF extends Component {
@@ -46,27 +58,16 @@ class StatisticsUIF extends Component {
     this.setState({datetime}, () => {
       this.loadUIF()  
     })
-
-    intervalId = setInterval(this.loadUIF.bind(this, true), 300000) // 5 mins
   }
   componentWillUnmount() {
-    clearInterval(intervalId)
+
   }
-  loadUIF(isAutoRefresh) {
+  loadUIF = () => {
     const {baseUrl, session} = this.context
     const url = `${baseUrl}/api/uif?id=SCP-Overview`
 
     let {datetime} = this.state
     let appendConfig = {}
-
-    if (isAutoRefresh) {
-      datetime = {
-        from: helper.getSubstractDate(1, 'days', Moment().local()),
-        to: Moment().local().format('YYYY-MM-DDTHH:mm:ss')
-      }
-
-      this.setState({datetime})
-    }
 
     this.ah.one({url})
     .then(data => {
@@ -129,7 +130,7 @@ class StatisticsUIF extends Component {
 
       })
 
-      this.setState({appendConfig})
+      this.setState({appendConfig, uifCfg})
     })
     .catch(err => {
       helper.showPopupMsg('', t('txt-error'), err.message)
@@ -137,6 +138,32 @@ class StatisticsUIF extends Component {
   }
   handleChange(field, value) {
     this.setState({[field]: value})
+  }
+  handleDateChange = (datetime, refresh) => {
+    this.setState({
+      datetime
+    }, () => {
+      if (refresh === 'refresh') {
+        this.loadUIF()
+      }
+    });
+  }
+  setSearchData = (type, value) => {
+    if (type === 'all') {
+      this.setState({
+        searchInput: value
+      });
+    } else {
+      let tempSearchInput = {...this.state.searchInput};
+
+      if (value) {
+        tempSearchInput[type] = value;
+
+        this.setState({
+          searchInput: tempSearchInput
+        });
+      }
+    }
   }
   onTooltip = (type, eventInfo, data) => {
     if (type === 'AlertStatistics-bar') {
@@ -160,20 +187,89 @@ class StatisticsUIF extends Component {
       </section>
     } 
   }
+  exportPDF() {
+    Progress.startSpin()
+
+    const {uifCfg} = this.state
+    let cfg = {
+      data: []
+    }    
+
+    cfg.data = _.map(uifCfg.config.widgets, (v, k) => {
+      return {
+        name: `${k}.jpg`,
+        width: v.layout.w * 2,
+        x: v.layout.x * 2,
+        y: v.layout.y,
+        type: 'image'
+      }
+    })
+
+    const {baseUrl, contextRoot} = this.context
+
+    Promise.all(_.map(uifCfg.config.widgets, (value, chart) => {
+      return htmlToImage.toPng(document.getElementById(chart))
+      .then(function(dataUrl) {
+          const imgArray = dataUrl.split(',')
+          const mime = imgArray[0].match(/:(.*?);/)[1]
+          const bstr = atob(imgArray[1])
+          let n = bstr.length
+          let u8arr = new Uint8Array(n)
+
+          while (n--) {
+            u8arr[n] = bstr.charCodeAt(n)
+          }
+          return new File([u8arr], `${chart}.jpg`, {type: mime})
+      })
+    }))
+    .then(files => {
+      let formData = new FormData()
+      formData.append('config_string', JSON.stringify(cfg))
+
+      _.forEach(files, file => {
+        formData.append('files', file)
+      })
+      
+      this.ah.one({
+        url: `${baseUrl}/api/pdf/_relay`,
+        data: formData,
+        type: 'POST',
+        dataType: 'JSON',
+        processData: false,
+        contentType: false
+      })
+      .then(data => {
+        downloadLink(`${baseUrl}${contextRoot}/api/pdf/_download`)
+      })
+      .catch(err => {
+        helper.showPopupMsg('', t('txt-error'), err.message)
+      })
+    })
+    .finally(() => {
+      Progress.done()
+    })
+  }
 	render() {
     const {locale} = this.context
-    const {appendConfig, datetime} = this.state
+    const {appendConfig, datetime, searchInput} = this.state
 
 		return <div>
 			<div className='sub-header'>
 				{helper.getDashboardMenu('statisticsUIF')}
-        <div style={{display: 'flex', float: 'right'}}>
-          <DateRange id='datetime' className='daterange' onChange={this.handleChange.bind(this, 'datetime')}
-            enableTime={true} value={datetime} locale={locale} t={et} />
-          <button style={{marginLeft: '5px'}} onClick={this.loadUIF.bind(this, false)}>{t('txt-query')}</button>
-        </div>
-			</div>
 
+        <div className='secondary-btn-group right'>
+          <button className='last' title={t('txt-export')} onClick={this.exportPDF.bind(this)} ><i className='fg fg-data-download'></i></button>
+        </div>
+
+        <SearchOptions
+          datetime={datetime}
+          searchInput={searchInput}
+          enableTime={true}
+          showInterval={true}
+          setSearchData={this.setSearchData}
+          handleDateChange={this.handleDateChange}
+          handleSearchSubmit={this.loadUIF} />
+      </div>
       {
         !_.isEmpty(appendConfig) &&
         <div className='uif-dashboard'>
