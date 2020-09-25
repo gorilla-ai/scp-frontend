d2fedad673f8346d6a29454dea363b0dc3aa5f5e
Ryan Chen (Telmediatech) <ryanchen@telmediatech.com>
2020-09-25 14:32:17 +0800

    Revert back the API ipdevice page size.

--- src/components/configuration/topology/inventory.js
+++ src/components/configuration/topology/inventory.js
@@ -1366,7 +1366,7 @@ class NetworkInventory extends Component {
     }
 
     this.ah.one({
-      url: `${baseUrl}/api/v2/ipdevice?uuid=${ipDeviceID}&page=1&pageSize=1`,
+      url: `${baseUrl}/api/v2/ipdevice?uuid=${ipDeviceID}&page=1&pageSize=5`,
       type: 'GET'
     })
     .then(data => {
