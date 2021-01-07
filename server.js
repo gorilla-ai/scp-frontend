80066ee7e67b3bde14eaf927408a29058b12e96a
Kenneth Chiao (Telmediatech) <kenneth.chiao@gorillascience.com>
2021-01-07 10:53:45 +0800

    change to default 119 server

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ const server = require('http').Server(app)
 const cfg = _.assign({
   env: process.env.NODE_ENV || 'development', 
   contextRoot: '',
-  // apiPrefix: 'http:\//172.18.0.119/SCP', //Prod environment
+  apiPrefix: 'http:\//172.18.0.119/SCP', //Prod environment
   // apiPrefix: 'http:\//192.168.11.91/SCP', //Test environment
   //apiPrefix: 'http:\//192.168.12.111/SCP', //QA environment
   //apiPrefix: 'http:\//172.18.0.136/SCP', //Rays's machine
   //apiPrefix: 'http:\//172.18.0.87/SCP', //Rex's machine
   //apiPrefix: 'http:\//172.18.0.52/SCP', //Allen's machine
-  apiPrefix: 'http:\//localhost:8080/SCP', //local machine
+  // apiPrefix: 'http:\//localhost:8080/SCP', //local machine
   port: process.env.PORT || 3000,
   version: '1.2.2437.491',
   lng: 'zh',
